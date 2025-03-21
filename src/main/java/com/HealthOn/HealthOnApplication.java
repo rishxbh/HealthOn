52f7a0886bb78fc11c214262337c0a3f31356d96
rishxbh <88018415+rishxbh@users.noreply.github.com>
2025-03-22 02:47:57 +0530

    initialCommit

---
+++ src/main/java/com/HealthOn/HealthOnApplication.java
@@ -0,0 +1,13 @@
+package com.HealthOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HealthOn/HealthOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HealthOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
